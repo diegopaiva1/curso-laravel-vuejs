bd070531290256b9c6cf6bde1b5866bb4c61fd16
Diego Paiva <diego.paiva13@gmail.com>
2018-04-22 00:56:23 -0300

    Componente do painel personalizado

---
+++ resources/assets/js/components/Painel.vue
@@ -0,0 +1,22 @@
+<template>
+    <div :class="setCardOptions">
+        <div class="card-header">{{ titulo }}</div>
+
+        <div class="card-body">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['titulo', 'backgroundColor', 'fontColor'],
+        computed:
+        {
+            setCardOptions: function() {
+                return "card text-" + this.fontColor + " bg-" + this.backgroundColor + " mb-3"
+            }
+        }
+    }
+</script>
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <painel titulo="Conteudo 1" font-color="white" background-color="dark">
+                    Este é o primeiro conteúdo!
+                </painel>
+            </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+            <div class="col-md-4">
+                <painel titulo="Conteudo 2" font-color="white" background-color="primary">
+                    Este é o segundo conteúdo!
+                </painel>
+            </div>
 
-                    You are logged in!
-                </div>
+            <div class="col-md-4">
+                <painel titulo="Conteudo 3" font-color="white" background-color="success">
+                    Este é o terceiro conteúdo!
+                </painel>
             </div>
         </div>
-    </div>
-</div>
+    </div> 
 @endsection
